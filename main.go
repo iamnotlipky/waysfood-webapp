34e4763dc3d8a94e8f4272e62322a9af5be6ca4a
iamrifqi <iamnotlipky@gmail.com>
2022-11-06 22:39:57 +0700

    Update Deploy Trouble

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"foodways/routes"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
@@ -33,6 +34,7 @@ func main() {
 	var allowedMethods = handlers.AllowedMethods([]string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"})
 	var allowedOrigins = handlers.AllowedOrigins([]string{"*"})
 
+	port := os.Getenv("PORT")
 	fmt.Println("server running on port 8000")
-	http.ListenAndServe(":8000", handlers.CORS(allowedHeaders, allowedMethods, allowedOrigins)(r))
+	http.ListenAndServe(":" + port, handlers.CORS(allowedHeaders, allowedMethods, allowedOrigins)(r))
 }
