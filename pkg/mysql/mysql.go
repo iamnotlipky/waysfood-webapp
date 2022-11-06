17b693e9ed2c367e36616797925389146a7d92ae
iamrifqi <iamnotlipky@gmail.com>
2022-11-06 22:43:47 +0700

    Update Deploy Postgres

--- pkg/mysql/mysql.go
+++ pkg/mysql/mysql.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,7 +12,14 @@ var DB *gorm.DB
 
 func DatabaseInit() {
 	var err error
-	dsn := "root:@tcp(localhost:3306)/b40_deploy?charset=utf8mb4&parseTime=True&loc=Local"
+
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
